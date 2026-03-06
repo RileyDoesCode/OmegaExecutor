a0e59aaa1e13acdb74586533414ca9b984d284d0
RileyDoesCode <enriquejoaquingreyes11@gmail.com>
2026-03-07 00:08:22 +0800

    Update actual_ui.lua

--- actual_ui.lua
+++ actual_ui.lua
@@ -1,11 +1,8 @@
---[=[
- d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
-88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
-88      88    88    88            odD'      88      88    88 88ooo88 
-88  ooo 88    88    88          .88'        88      88    88 88~~~88 
-88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88 
- Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER
-]=]
+if not isfile("new_logo.png") then
+local new_logo = game:HttpGetAsync("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/lxnnypfp7.png")
+writefile("new_logo.png", new_logo)
+end
+
 makefolder("d_android_script_dir")
 
 if _G.is_omegaandroid_loaded then
@@ -45,7 +42,7 @@ G2L["4"]["Name"] = [[SideFrame]];
 -- StarterGui.OmegaGui.KeySystem.MainKeyFrame.SideFrame.Logo
 G2L["5"] = Instance.new("ImageLabel", G2L["4"]);
 G2L["5"]["BackgroundColor3"] = Color3.fromRGB(143, 130, 255);
-G2L["5"]["Image"] = [[rbxassetid://99733751052578]];
+G2L["5"]["Image"] = getcustomasset("new_logo.png")
 G2L["5"]["Size"] = UDim2.new(1.0185703039169312, 0, 0.3629874587059021, 0);
 G2L["5"]["Name"] = [[Logo]];
 G2L["5"]["BackgroundTransparency"] = 1;
@@ -2032,7 +2029,7 @@ G2L["132"]["Name"] = [[DraggableStuff]];
 -- StarterGui.OmegaGui.FloatingIcon
 G2L["133"] = Instance.new("ImageButton", G2L["1"]);
 G2L["133"]["BackgroundColor3"] = Color3.fromRGB(20, 20, 23);
-G2L["133"]["Image"] = [[rbxassetid://99733751052578]];
+G2L["133"]["Image"] = getcustomasset("new_logo.png")
 G2L["133"]["Size"] = UDim2.new(0.07388234883546829, 0, 0.1319999247789383, 0);
 G2L["133"]["Name"] = [[FloatingIcon]];
 G2L["133"]["Visible"] = false;
